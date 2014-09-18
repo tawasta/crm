b01677ad9c3beae7954fe8b9e0ca84f025201f36
Jarmo Kortetjärvi <jarmo@futurable.fi>
2014-09-18 11:22:07 +0300

    Fix a relation

--- claim.py
+++ claim.py
@@ -45,7 +45,7 @@ class crm_claim(osv.Model):
     def _default_get_reply_to(self, cr, uid, context=None):
         reply_ids=self.pool.get('crm_claim.reply').search(cr,uid,[])
         if reply_ids:
-            for reply_obj in self.pool.get('crm_claim.reply').browse(cr,uid,reply_ids):
+            for reply_obj in self.pool.get('crm_claim.reply_to').browse(cr,uid,reply_ids):
                 reply_to=reply_obj.reply_to
                 return reply_to
         return False
