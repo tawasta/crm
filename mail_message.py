398a1992886123a09d6e3b8c56d21e1ce49a827e
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-01-09 11:43:36 +0200

    Fix a "boomerang" message to self when partner didn't exist

--- mail_message.py
+++ mail_message.py
@@ -59,20 +59,35 @@ class mail_mail(osv.Model):
 class mail_message(osv.Model):
     _inherit = 'mail.message'
     
-    '''
     def create(self, cr, uid, values, context=None):
         if context is None:
             context = {}
         model = context.get('thread_model')
 
         if model and model=='crm.claim':
-            res_id =  context.get('default_res_id')
-            claim = self.pool[model].browse(cr,uid,res_id)
-            
-            if 'reply_to' not in values:
-                values['reply_to'] = claim.reply_to if claim.reply_to else False
+            ''' TODO: Get rid of this hack.
+                For some reason the author_id is False if new partner was created
+            '''
+            if values.get('author_id') == False:
+                email_from = values.get('email_from')
+                email_regex = re.compile("[<][^>]+[>]")
+                
+                email = email_regex.findall(email_from)[0]
+                email = re.sub(r'[<>]', "", email)
 
-        newid = super(mail_message, self).create(cr, uid, values, context)
+                values['author_id'] = self.get_author_by_email(cr, uid, values, context)
+                
+            
+        return super(mail_message, self).create(cr, uid, values, context=context)
     
-        return newid
-    '''
+    def get_author_by_email(self, cr, uid, values, context=None):
+        email_from = values.get('email_from')
+        email_regex = re.compile("[<][^>]+[>]")
+        
+        email = email_regex.findall(email_from)[0]
+        email = re.sub(r'[<>]', "", email)
+    
+        author = self.pool.get('res.partner').search(cr, uid, [('email','=',email)])[0]
+        
+        return author
+        
