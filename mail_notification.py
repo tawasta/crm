649d6dc846d4651b64235a4de8b488b1e9a4dfa1
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-01-28 10:59:17 +0200

    Fix the scope

--- mail_notification.py
+++ mail_notification.py
@@ -17,12 +17,11 @@ class mail_notification(osv.Model):
         notify_pids = super(mail_notification, self).get_partners_to_email(cr, uid, ids, message, context)
         
         ''' Send helpdesk messages even if partner has notifications disabled '''
-        for notification in self.browse(cr, uid, ids, context=context):
-            partner = notification.partner_id
-            
-            if partner.notify_email == 'none':
-                notify_pids.append(partner.id)
-        
-        _logger.warn(notify_pids)
+        if message.model == "crm.claim":
+            for notification in self.browse(cr, uid, ids, context=context):
+                partner = notification.partner_id
+                
+                if partner.notify_email == 'none':
+                    notify_pids.append(partner.id)
         
         return notify_pids
