dcdb42548843e1d7790bdefe054b4b4748da98f6
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-04-10 14:03:08 +0300

    Add default structure

---
+++ __openerp__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#   Copyright (c) 2015- Vizucom Oy (http://www.vizucom.com)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Partner Address Customizations',
+    'category': 'CRM',
+    'version': '0.1',
+    'author': 'Vizucom Oy',
+    'website': 'http://www.vizucom.com',    
+    'depends': ['crm', 'sale_business_id', 'crm_customer_account_number_gen'],
+    'description': '''
+* Simplifies res_partner address types
+* Simplifies adding addresses
+* Breaks res_partner addresses to multiple notebook pages
+* Changes res_partner address views from kanban to row
+''',
+    'data': [
+        'view/res_partner_form.xml',
+        'view/res_partner_menu.xml',
+        'view/res_partner_contact_form.xml',
+    ],
+}
---
+++ res_partner.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields, api, _
+from openerp.tools.translate import _
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class ResPartner(models.Model):
+    
+    _inherit = 'res.partner'
+
+    TYPES_ARRAY = ( ('contact', _('Contact')), ('delivery', _('Shipping')), ('einvoice', _('eInvoice')))
+    
+    '''
+    def _get_full_name(self, cr, uid, ids, field_name, arg, context=None):
+        records = self.browse(cr, uid, ids)
+        result = {}
+        for record in records:
+            if hasattr(record, 'lvl2_name') and hasattr(record, 'lvl3_name'):
+                result[record.id] = record.lvl2_name + " / " + record.lvl3_name + " / " + record.name
+            else:
+                result[record.id] = record.name
+        
+        return result
+    
+    def _get_contacts(self, cr, uid, ids, name, arg, context=None):
+        record = self.browse(cr, uid, ids, context=context)[0]
+        
+        result = {}
+        records = self.browse(cr, uid, ids)
+        
+        # Fetch two levels of children
+        domain = ['&', '|',('parent_id','=',record.id),('parent_id.parent_id','=',record.id),('type', '=', 'contact')]
+        contact_ids = self.search(cr, uid, args=domain, context=context)
+        
+        for record in records:
+            result[record.id] = contact_ids
+        
+        return result
+    
+    # fnct_inv for dealing with saving functional field values
+    def _set_contacts(self, cr, uid, ids, name, field_values, inv_arg, context):
+        partner_obj = self.pool.get('res.partner')
+        
+        # Iterate through all of the document ids and see if they've been
+        #  flagged by OE for create, write or unlink
+        # See http://help.openerp.com/question/28714/how-to-insert-value-to-a-one2many-field-in-table-with-create-method/?answer=28771#post-id-28771
+        for line in field_values:
+            if line[0] == 0: # add
+                line_id = line[1]
+                partner_obj.create(cr, uid, line[2], context)
+            elif line[0] == 1: # update
+                line_id = line[1]
+                partner_obj.write(cr, uid, [line_id], line[2], context)                
+            elif line[0] == 2: # delete
+                line_id = line[1]
+                partner_obj.unlink(cr, uid, [line_id], context)
+        return True
+    '''
+
+    ''' Columns '''
+    type = fields.Selection(TYPES_ARRAY, 'Address Type')
+                
+    #'address_contact_recursive_ids': fields.One2many(_get_contacts, fnct_inv=_set_contacts, relation="res.partner", method=True, type="one2many", string=_("Contacts")),
+    address_contact_recursive_ids = fields.One2many('res.partner', 'parent_id', string=_('e-Invoice'), domain=[('type', '=', 'contact')])
+    address_einvoice_ids = fields.One2many('res.partner', 'parent_id', string=_('e-Invoice'), domain=[('type', '=', 'einvoice')])
+    address_delivery_ids = fields.One2many('res.partner', 'parent_id', string=_('Delivery'), domain=[('type', '=', 'delivery')])
+        
+    edicode = fields.Char(string='Edicode')
+    einvoice_operator = fields.Char(string='eInvoice operator')
+    
