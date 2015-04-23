426ab3b13219c9a7ef4aeba794926bd3a927189e
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-04-23 15:18:57 +0300

    Add setters

--- res_partner.py
+++ res_partner.py
@@ -17,6 +17,10 @@ class ResPartner(models.Model):
         for record in self:
             record.display_name = self._get_recursive_name(record)
     
+    def _set_display_name(self):
+        for record in self:
+            record.display_name = self._get_recursive_name(record)
+    
     def _get_recursive_name(self, record):
         ''' Returns a recursive partner name '''
         
@@ -30,7 +34,6 @@ class ResPartner(models.Model):
     def _get_contacts(self):
         for record in self:
             child_ids = self._get_recursive_child_ids(record)
-            _logger.warn(child_ids)
             record.address_contact_recursive_ids = self.search(['&',('id','in', child_ids ), ('type','=', 'contact')])
 
     ''' TODO: this function might be very heavy with large customer bases '''
@@ -44,34 +47,19 @@ class ResPartner(models.Model):
                 child_ids += self._get_recursive_child_ids(child)
 
         return child_ids
-    
-    '''
-    # fnct_inv for dealing with saving functional field values
-    def _set_contacts(self, cr, uid, ids, name, field_values, inv_arg, context):
-        partner_obj = self.pool.get('res.partner')
-        
-        # Iterate through all of the document ids and see if they've been
-        #  flagged by OE for create, write or unlink
-        # See http://help.openerp.com/question/28714/how-to-insert-value-to-a-one2many-field-in-table-with-create-method/?answer=28771#post-id-28771
-        for line in field_values:
-            if line[0] == 0: # add
-                line_id = line[1]
-                partner_obj.create(cr, uid, line[2], context)
-            elif line[0] == 1: # update
-                line_id = line[1]
-                partner_obj.write(cr, uid, [line_id], line[2], context)                
-            elif line[0] == 2: # delete
-                line_id = line[1]
-                partner_obj.unlink(cr, uid, [line_id], context)
-        return True
-    '''
+
+    def _set_contacts(self):
+        for record in self:
+            for contact in record.address_contact_recursive_ids:
+                if isinstance(contact.id, models.NewId):
+                    contact_id = self.create({'name': contact.name})
+                    #record.address_contact_recursive_ids = {(4, record.id, contact_id.id)}
 
     ''' Columns '''
     type = fields.Selection(TYPES_ARRAY, 'Address Type')
-    display_name = fields.Char(string='Name', compute='_get_display_name')
+    display_name = fields.Char(string='Name', compute='_get_display_name', inverse='_set_display_name')
                 
-    #'address_contact_recursive_ids': fields.One2many(_get_contacts, fnct_inv=_set_contacts, relation="res.partner", method=True, type="one2many", string=_("Contacts")),
-    address_contact_recursive_ids = fields.One2many('res.partner', 'parent_id', string=_('Contact'), compute='_get_contacts')
+    address_contact_recursive_ids = fields.One2many('res.partner', 'parent_id', string=_('Contact'), compute='_get_contacts', inverse='_set_contacts')
     address_einvoice_ids = fields.One2many('res.partner', 'parent_id', string=_('e-Invoice'), domain=[('type', '=', 'einvoice')])
     address_delivery_ids = fields.One2many('res.partner', 'parent_id', string=_('Delivery'), domain=[('type', '=', 'delivery')])
         
