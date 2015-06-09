f5ad767c58fb98cd0c890436c05eaee3ca3f43db
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-06-09 13:33:10 +0300

    Add 'is_company'-helper

--- res_partner.py
+++ res_partner.py
@@ -5,4 +5,21 @@ class ResPartner(models.Model):
     
     _inherit = 'res.partner'
     
-    show_all= fields.Boolean('Show all fields')
+    show_all = fields.Boolean('Show all fields')
+    
+    # This field is only a helper for "is company"
+    retail_customer = fields.Boolean('Retail Customer')
+    
+    @api.onchange('retail_customer')
+    def retail_customer_onchange(self):
+        if self.retail_customer:
+            self.is_company = False
+        else:
+            self.is_company = True
+            
+    @api.onchange('is_company')
+    def is_company_onchange(self):
+        if self.is_company:
+            self.retail_customer = False
+        else:
+            self.retail_customer = True
