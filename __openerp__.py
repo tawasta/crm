9e65112a438d389f12f1d1be29fcee130a2e67ec
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-06-12 13:03:33 +0300

    Comment out the correct view

--- __openerp__.py
+++ __openerp__.py
@@ -38,10 +38,10 @@ Features
     'data': [
         'view/res_partner_form.xml',
         'view/res_partner_form_contact.xml',
-        'view/res_partner_form_affiliate.xml',
+        #'view/res_partner_form_affiliate.xml',
         'view/res_partner_form_einvoice.xml',
         'view/res_partner_tree_contact.xml',
-        #'view/res_partner_tree_affiliate.xml',
+        'view/res_partner_tree_affiliate.xml',
         'view/res_partner_tree_einvoice.xml',
         #'view/res_partner_tree.xml',
         'view/res_partner_menu.xml',
