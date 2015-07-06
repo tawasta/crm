3d1aff9120fa3b30ba60a4c9e262fc45a88e2f59
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-07-06 13:45:01 +0300

    Add a name setter for updating children display names

--- res_partner.py
+++ res_partner.py
@@ -23,24 +23,30 @@ class ResPartner(models.Model):
     TYPES_ARRAY = (('contact', _('Contact')), ('delivery', _('Affiliate')), ('invoice', _('e-Invoice address')))
     
     @api.one
-    def name_get(self):
-        return self.id, self._get_recursive_name(self)
-    
-    @api.one
     @api.depends('name', 'parent_id.name')
     def _get_display_name(self):
         # Returns a name with a complete hierarchy
 
         self.display_name = self._get_recursive_name(self)
+        
+    @api.one
+    def _set_name(self):
+        # Updates all children display names
+        
+        child_ids = self._get_recursive_child_ids(self)
+        children = self.search([('id', 'in', child_ids)])
+        
+        for record in children:
+            record.display_name = self._get_recursive_name(record)
     
     def _get_recursive_name(self, record):
         # Returns a recursive partner name
         
-        if not record.parent_id:
-            record.display_name = record.name
-        else:
+        if record.parent_id:
             record.display_name = "%s, %s" % (self._get_recursive_name(record.parent_id), record.name)
-        
+        else:
+            record.display_name = record.name
+            
         return record.display_name
     
     def _get_contacts(self):
@@ -101,6 +107,7 @@ class ResPartner(models.Model):
     ''' Columns '''
     type = fields.Selection(TYPES_ARRAY, 'Address Type')
     
+    name = fields.Char(inverse='_set_name')
     display_name = fields.Char(compute='_get_display_name')
                     
     address_contact_recursive_ids = fields.One2many('res.partner', 'parent_id', string=_('Contact'), compute='_get_contacts', inverse='_set_contacts')
