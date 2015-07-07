2b8d53c8dfe50badaa51a8f98f7890150b849433
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-07-07 12:09:01 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+CRM Claim Extension Reports
+--------------------------
+
+Overwrites the default claim reports, as they are just plain wrong with exteneded claims
+
+
+Features
+--------
+* None Yet
---
+++ __openerp__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#   Copyright (c) 2015 - Oy Tawasta Technologies Ltd. (http://www.tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'CRM Claim Extension Reports',
+    'category': 'CRM',
+    'version': '0.1',
+    'author': 'Oy Tawasta Technologies Ltd.',
+    'website': 'http://www.tawasta.fi',
+    'depends': ['crm', 'sale_business_id', 'crm_customer_account_number_gen'],
+    'description': '''
+CRM Claim Extension Reports
+--------------------------
+
+Overwrites the default claim reports, as they are just plain wrong with exteneded claims
+
+
+Features
+--------
+* None Yet
+
+''',
+    'data': [
+
+    ],
+}
