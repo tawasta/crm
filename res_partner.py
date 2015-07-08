7ce63916fafd2e9ce43a2ea68ada8010d37e5881
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-07-08 10:03:18 +0300

    Refactor to follow PEP8

--- res_partner.py
+++ res_partner.py
@@ -1,22 +1,24 @@
 # -*- coding: utf-8 -*-
 from openerp import models, api, fields
 
+
 class ResPartner(models.Model):
-    
+
     _inherit = 'res.partner'
-    
+
     show_all = fields.Boolean('Show all fields')
-    
+
     # This field is only a helper for "is company"
-    personal_customer = fields.Boolean('Personal Customer', help="A customer that's not a company")
-    
+    personal_customer = fields.Boolean('Personal Customer',
+                                       help="A customer that's not a company")
+
     @api.onchange('personal_customer')
     def personal_customer_onchange(self):
         if self.personal_customer:
             self.is_company = False
         else:
             self.is_company = True
-            
+
     @api.onchange('is_company')
     def is_company_onchange(self):
         if self.is_company:
