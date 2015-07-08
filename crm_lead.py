9b3375509bf4cb9c0957cc93b7d02cae79de1e72
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-07-08 10:04:37 +0300

    Refactor to follow PEP8

--- crm_lead.py
+++ crm_lead.py
@@ -2,7 +2,8 @@
 from openerp import models, fields, api, _
 from openerp.tools.translate import _
 
+
 class CrmLead(models.Model):
     _inherit = "crm.lead"
-    
+
     sale_order_id = fields.Many2one('sale.order', "Related sale")
--- crm_make_sale.py
+++ crm_make_sale.py
@@ -5,18 +5,19 @@ from openerp.tools.translate import _
 import logging
 _logger = logging.getLogger(__name__)
 
+
 class CrmMakeSale(models.TransientModel):
     _inherit = "crm.make.sale"
-    
+
     @api.multi
     def makeOrder(self):
         res = super(CrmMakeSale, self).makeOrder()
-        
+
         if res['res_model'] == 'sale.order':
-            sale_order = self.env['sale.order'].browse( res['res_id'] )
+            sale_order = self.env['sale.order'].browse(res['res_id'])
             lead = self.env['crm.lead'].browse(self._context['active_id'])
-            
+
             sale_order.lead_id = lead.id
             lead.sale_order_id = sale_order.id
-        
+
         return res
