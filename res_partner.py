3d3a119284c62d1296ae0e48be9f82d001a1bf83
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-07-08 10:10:48 +0300

    Refactor to follow PEP8

--- res_partner.py
+++ res_partner.py
@@ -2,14 +2,15 @@
 from openerp import models, fields, api, _
 from openerp.tools.translate import _
 
+
 class Partner(models.Model):
-    
+
     _inherit = 'res.partner'
-    
+
     @api.multi
     def unlink(self):
         ''' Deactivates the partner instead of deleting '''
-        
+
         self.active = False
-        
+
         return True
