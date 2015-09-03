de1516038fa31521fb0ae15ef4a51e1cd44f4657
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-09-03 10:36:07 +0300

    Improve PEP8 compatibility

--- fetchmail_server.py
+++ fetchmail_server.py
@@ -1,11 +1,13 @@
 from openerp.osv import osv, fields
 
-class mail_thread(osv.Model):
-    ''' Add a company for fetchmail servers so we can match the claims for a correct company '''
-    _inherit = 'fetchmail.server'
-    
-    _columns = {
-        'company_id': fields.many2one('res.company', string='Company', required=True),
-    }
 
-    
+class mail_thread(osv.Model):
+    ''' Add a company for fetchmail servers so we can match
+    the claims for a correct company '''
+    _inherit = 'fetchmail.server'
+
+    _columns = {
+        'company_id': fields.many2one(
+            'res.company', string='Company', required=True
+        ),
+    }
