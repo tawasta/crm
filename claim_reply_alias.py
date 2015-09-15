d18bc075983cdafc2c331d1b92f42ffb771725b8
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-09-15 15:49:54 +0300

    Improve pep8 compatibility

--- claim_reply_alias.py
+++ claim_reply_alias.py
@@ -1,12 +1,13 @@
 from openerp.osv import osv, fields
 
+
 class crm_claim_reply_alias(osv.osv):
     ''' Claim reply alias to remove support emails from claim CC-field.'''
     ''' NOT IMPLEMENTED '''
-    
-    _name='crm_claim.reply.alias'
-    
-    _columns={
+
+    _name = 'crm_claim.reply.alias'
+
+    _columns = {
         'reply_id': fields.many2one('crm_claim.reply', 'Reply id'),
         'name': fields.char('Address'),
         'description': fields.char('Description'),
