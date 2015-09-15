2e87a0ab05d2e5935f0748108463d7936c6bbd3e
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-09-15 15:51:29 +0300

    Improve pep8 compatibility

--- claim_stage_change.py
+++ claim_stage_change.py
@@ -1,17 +1,15 @@
 from openerp.osv import osv, fields
 from openerp.tools.translate import _
 
-class ClaimStageChange(osv.Model):      
 
-    _name='crm.claim.stage.change'
-    _rec_name='stage'
-    
-    _order='create_date DESC'
-    
+class ClaimStageChange(osv.Model):
+
+    _name = 'crm.claim.stage.change'
+    _rec_name = 'stage'
+
+    _order = 'create_date DESC'
+
     _columns = {
         'stage': fields.many2one('crm.claim.stage', _('New stage')),
         'claim_id': fields.many2one('crm.claim', _('Claim id')),
     }
-
-    
-    
