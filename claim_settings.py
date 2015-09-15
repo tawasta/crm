887ffdb47fd457017c326b90d74ca676abb6aa91
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-09-15 15:51:11 +0300

    Improve pep8 compatibility

--- claim_settings.py
+++ claim_settings.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from openerp.osv import osv, fields
 
+
 class claim_settings(osv.osv):
     ''' A model for storing the claim settings (next number) '''
-    
+
     _name = "crm_claim.settings"
     _rec_name = 'next_number'
-    
+
     _columns = {
         'next_number': fields.integer('The next claim number'),
     }
-    
