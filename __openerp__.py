bd33324261b726778af7e5857c565502962450c6
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-09-17 12:45:09 +0300

    Improve version numbering

--- __openerp__.py
+++ __openerp__.py
@@ -15,7 +15,7 @@
 {
     'name': 'CRM Address Simplification',
     'category': 'CRM',
-    'version': '0.1',
+    'version': '8.0.0.2.0',
     'author': 'Oy Tawasta Technologies Ltd.',
     'website': 'http://www.tawasta.fi',
     'depends': ['crm', 'sale_business_id', 'crm_customer_account_number_gen'],
