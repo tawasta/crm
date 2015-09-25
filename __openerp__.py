8229f67e7778e6136014bb063dcc8b25330dd2d5
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-09-25 10:08:28 +0300

    Move model to models dir

--- __openerp__.py
+++ __openerp__.py
@@ -21,7 +21,7 @@
 {
     'name': 'CRM Claim Extension Reports',
     'category': 'CRM',
-    'version': '8.0.0.1.2',
+    'version': '8.0.0.1.3',
     'author': 'Oy Tawasta Technologies Ltd.',
     'website': 'http://www.tawasta.fi',
     'depends': ['crm_claim_extension'],
