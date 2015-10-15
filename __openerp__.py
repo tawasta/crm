28e45c6f6b7f9a177d3c9c17f7c3d6edcad5775d
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-10-15 10:24:57 +0300

    Improve formatting

--- __openerp__.py
+++ __openerp__.py
@@ -1,21 +1,27 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#   Copyright (c) 2015 - Oy Tawasta Technologies Ltd. (http://www.tawasta.fi)
+#    Author: Jarmo Kortetjärvi
+#    Copyright 2015 Oy Tawasta OS Technologies Ltd.
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
 #    published by the Free Software Foundation, either version 3 of the
 #    License, or (at your option) any later version.
 #
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 {
     'name': 'CRM Address Simplification',
     'category': 'CRM',
-    'version': '8.0.0.3.3',
+    'version': '8.0.0.3.5',
     'author': 'Oy Tawasta Technologies Ltd.',
     'website': 'http://www.tawasta.fi',
     'depends': ['crm', 'sale_business_id', 'crm_customer_account_number_gen'],
