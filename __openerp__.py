1fd07f948a45c71f2901fee9f4e6eb7dba927c1e
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-10-22 14:55:00 +0300

    Fix title visibility

--- __openerp__.py
+++ __openerp__.py
@@ -21,7 +21,7 @@
 {
     'name': 'CRM Simplification',
     'category': 'CRM',
-    'version': '8.0.0.4.3',
+    'version': '8.0.0.4.4',
     'author': 'Oy Tawasta Technologies Ltd.',
     'website': 'http://www.tawasta.fi',
     'depends': [
