8ef927ca6576849dd842d64969a39d92205407ca
Jarmo Kortetjarvi <jarmo@futurable.fi>
2015-11-10 09:47:13 +0200

    Add translations

--- __openerp__.py
+++ __openerp__.py
@@ -21,7 +21,7 @@
 {
     'name': 'CRM Address Simplification',
     'category': 'Customer Relationship Management',
-    'version': '8.0.0.3.12',
+    'version': '8.0.0.3.13',
     'author': 'Oy Tawasta Technologies Ltd.',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
