bc9f86115eee0152f5cbf823ae118b1a0169656d
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2016-10-13 14:27:03 +0300

    Fix a reference

--- crm_customer_label_print/__openerp__.py
+++ crm_customer_label_print/__openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Customer address labels',
     'summary': 'Customer address labels print 3x8 without margins',
-    'version': '8.0.0.2.3',
+    'version': '8.0.0.2.4',
     'category': 'CRM',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
