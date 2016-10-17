d4bea32fe5f858dabf9e6368b02a22a938b43508
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2016-10-17 17:59:38 +0300

    Add a total field

--- crm_claim_extension/__openerp__.py
+++ crm_claim_extension/__openerp__.py
@@ -22,7 +22,7 @@
     'name': 'CRM Claims extension',
     'summary': 'Improved claims with helpdesk-like functionality',
     'category': 'Sales',
-    'version': '8.0.0.8.4',
+    'version': '8.0.0.8.5',
     'author': 'Oy Tawasta Technologies Ltd.',
     'website': 'http://www.tawasta.fi',
     'license': 'AGPL-3',
