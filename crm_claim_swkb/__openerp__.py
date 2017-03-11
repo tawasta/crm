eb3dbc580e6c8ca3c6201e7e35cf4a424fa2d4c5
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-03-11 21:51:23 +0200

    Remove editable tree

--- crm_claim_swkb/__openerp__.py
+++ crm_claim_swkb/__openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Claim software knowledge base records',
     'summary': 'Link claims to software knowledge base',
-    'version': '8.0.0.3.2',
+    'version': '8.0.0.3.3',
     'category': 'CRM',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
