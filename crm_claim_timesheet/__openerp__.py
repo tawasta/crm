12eddd1ce2714056d7f5603e6cd1d288dc148d90
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-04-07 14:42:46 +0300

    Remove an obsolete field

--- crm_claim_timesheet/__openerp__.py
+++ crm_claim_timesheet/__openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Claim timesheets',
     'summary': 'Timesheet records from claims',
-    'version': '8.0.0.4.5',
+    'version': '8.0.0.4.6',
     'category': 'Human Resources',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
