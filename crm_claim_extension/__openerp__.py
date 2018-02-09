92794d730ca8182f40fb4155da16b8d334069e01
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-02-09 10:24:47 +0200

    Add waiting tag to form view

--- crm_claim_extension/__openerp__.py
+++ crm_claim_extension/__openerp__.py
@@ -22,7 +22,7 @@
     'name': 'CRM Claims extension',
     'summary': 'Improved claims with helpdesk-like functionality',
     'category': 'Sales',
-    'version': '8.0.0.13.13',
+    'version': '8.0.0.13.14',
     'author': 'Oy Tawasta Technologies Ltd.',
     'website': 'http://www.tawasta.fi',
     'license': 'AGPL-3',
