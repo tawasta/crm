130a38572b2195f55148cbc3dd0eaf97ac5e8315
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-09-19 13:59:41 +0300

    Remove module permanently (deprecated: will never be forward-ported)

--- crm_tokenkey/res_partner.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from openerp import models, fields, api, _
-
-class ResPartner(models.Model):
-    
-    _inherit = 'res.partner'
-    
-    tokenkey_ids = fields.One2Many('crm.tokenkey')
