2c20f6b0eff1f35b78132582b68f9fb205236a01
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2020-05-20 09:47:50 +0300

    [IMP] Replace "Log Activity" with calendar event

--- crm_opportunity_calendar_event/__manifest__.py
+++ crm_opportunity_calendar_event/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Replace opportunity actions with calendar events',
     'summary': 'Replace opportunity actions with calendar events',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.1.0',
     'category': 'CRM',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
