e0a3ca41c8ec0243b7ba98d916011fff9b2e3e66
retropikzel <retropikzel@mailbox.org>
2020-05-20 10:00:31 +0300

    [FIX] Merge conflicts

--- crm_opportunity_calendar_event/models/crm_lead.py
+++ crm_opportunity_calendar_event/models/crm_lead.py
@@ -1,21 +1,4 @@
-<<<<<<< HEAD
-
-# 1. Standard library imports:
-
-# 2. Known third party imports:
-
-# 3. Odoo imports (openerp):
 from odoo import fields, models
-
-
-# 4. Imports from Odoo modules:
-
-# 5. Local imports in the relative form:
-
-# 6. Unknown third party imports:
-=======
-from odoo import fields, models
->>>>>>> 12.0
 
 
 class CrmLead(models.Model):
