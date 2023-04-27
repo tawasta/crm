5e8ecf5d2e916fa6de528431df9191926c99ce8f
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2023-04-27 12:38:08 +0300

    [ADD] Sorting to deadline all-field

--- crm_lead_activity_deadline_all/models/crm_lead.py
+++ crm_lead_activity_deadline_all/models/crm_lead.py
@@ -24,6 +24,7 @@
 
 # 3. Odoo imports (openerp):
 from odoo import api, fields, models
+from odoo.osv import expression
 
 # 4. Imports from Odoo modules:
 
@@ -74,6 +75,86 @@ class CrmLead(models.Model):
 
     def _search_activity_date_deadline_all(self, operator, operand):
         return [("activity_ids.date_deadline", operator, operand)]
+
+    @api.model
+    def search(self, args, offset=0, limit=None, order=None, count=False):
+        """Override to support ordering on activity_date_deadline_all"""
+        if count or not order or "activity_date_deadline_all" not in order:
+            return super().search(
+                args, offset=offset, limit=limit, order=order, count=count
+            )
+        order_items = [
+            order_item.strip().lower()
+            for order_item in (order or self._order).split(",")
+        ]
+
+        # Perform a read_group on all activities to get a mapping lead_id / deadline
+        # Remember date_deadline is required, we always have a value for it. Only
+        # the earliest deadline per lead is kept.
+        activity_asc = any(
+            "activity_date_deadline_all asc" in item for item in order_items
+        )
+        all_lead_activities = self.env["mail.activity"].read_group(
+            [("res_model", "=", self._name)],
+            ["res_id", "date_deadline:min"],
+            ["res_id"],
+            orderby="date_deadline ASC",
+        )
+        all_lead_mapping = {
+            item["res_id"]: item["date_deadline"] for item in all_lead_activities
+        }
+        all_lead_ids = list(all_lead_mapping.keys())
+        all_lead_domain = expression.AND([[("id", "in", all_lead_ids)], args])
+        all_lead_order = ", ".join(
+            item for item in order_items if "activity_date_deadline_all" not in item
+        )
+
+        # Search leads linked to those activities and order them. See docstring
+        # of this method for more details.
+        search_res = super().search(
+            all_lead_domain, offset=0, limit=None, order=all_lead_order, count=count
+        )
+        all_lead_ids_ordered = sorted(
+            search_res.ids,
+            key=lambda lead_id: all_lead_mapping[lead_id],
+            reverse=not activity_asc,
+        )
+        # keep only requested window (offset + limit, or offset+)
+        all_lead_ids_keep = (
+            all_lead_ids_ordered[offset : (offset + limit)]
+            if limit
+            else all_lead_ids_ordered[offset:]
+        )
+        # keep list of already skipped lead ids to exclude them from future search
+        all_lead_ids_skip = (
+            all_lead_ids_ordered[: (offset + limit)] if limit else all_lead_ids_ordered
+        )
+
+        # do not go further if limit is achieved
+        if limit and len(all_lead_ids_keep) >= limit:
+            return self.browse(all_lead_ids_keep)
+
+        # Fill with remaining leads. If a limit is given, simply remove count of
+        # already fetched. Otherwise keep none. If an offset is set we have to
+        # reduce it by already fetch results hereabove. Order is updated to exclude
+        # activity_date_deadline_all when calling super() .
+        lead_limit = (limit - len(all_lead_ids_keep)) if limit else None
+        if offset:
+            lead_offset = max((offset - len(search_res), 0))
+        else:
+            lead_offset = 0
+        lead_order = ", ".join(
+            item for item in order_items if "activity_date_deadline_all" not in item
+        )
+
+        other_lead_res = super().search(
+            expression.AND([[("id", "not in", all_lead_ids_skip)], args]),
+            offset=lead_offset,
+            limit=lead_limit,
+            order=lead_order,
+            count=count,
+        )
+        return self.browse(all_lead_ids_keep) + other_lead_res
 
     # 5. Constraints and onchanges
 
