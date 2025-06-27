c20dcfde6d55d11353daf294a10f8cb78c17e539
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2025-06-27 08:51:02 +0300

    [IMP] - created new modules

---
+++ crm_lead_activity_deadline_tree/models/crm_lead.py
@@ -0,0 +1,12 @@
+from odoo import models, fields, api
+
+class CrmLead(models.Model):
+    _inherit = 'crm.lead'
+
+    activity_date_deadline_stored = fields.Date(string="Next Activity Deadline (Stored)", compute="_compute_deadline", store=True)
+
+    @api.depends('activity_ids.date_deadline', 'activity_ids.date_done', 'activity_ids')
+    def _compute_deadline(self):
+        for lead in self:
+            dates = lead.activity_ids.filtered(lambda a: not a.date_done).mapped('date_deadline')
+            lead.activity_date_deadline_stored = min(dates) if dates else False
