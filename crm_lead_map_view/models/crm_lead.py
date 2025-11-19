0e9a889954ce4d857e31c26c593859ebbe8defd4
joona-isoaho-futural <joona.isoaho@futural.fi>
2025-11-19 09:11:49 +0200

    Pre-commit fixes

--- crm_lead_map_view/models/crm_lead.py
+++ crm_lead_map_view/models/crm_lead.py
@@ -5,8 +5,7 @@ class CRMLead(models.Model):
     _inherit = "crm.lead"
 
     customer_latitude = fields.Float(
-        string="Latitude",
-        related="partner_id.partner_latitude"
+        string="Latitude", related="partner_id.partner_latitude"
     )
     customer_longitude = fields.Float(
         string="Longitude", related="partner_id.partner_longitude"
