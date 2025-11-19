5ebc7cdd351cdeec32ddbf5bf65e773bfe8bd625
joona-isoaho-futural <joona.isoaho@futural.fi>
2025-11-19 07:52:27 +0200

    Map view made

---
+++ crm_lead_map_view/static/src/js/crm_lead_map_view.esm.js
@@ -0,0 +1,33 @@
+/** @odoo-module */
+
+import {CRMLeadMapArchParser} from "./crm_lead_map_arch_parser.esm";
+import {CRMLeadMapController} from "./crm_lead_map_controller.esm";
+import {CRMLeadMapModel} from "./crm_lead_map_model.esm";
+import {CRMLeadMapRenderer} from "./crm_lead_map_renderer.esm";
+import {registry} from "@web/core/registry";
+
+export const CRMLeadMapView = {
+    type: "CRMLeadMapView",
+    display_name: "Map",
+    icon: "fa fa-picture-o",
+    multiRecord: true,
+    Controller: CRMLeadMapController,
+    ArchParser: CRMLeadMapArchParser,
+    Model: CRMLeadMapModel,
+    Renderer: CRMLeadMapRenderer,
+
+    props(genericProps, view) {
+        const {ArchParser} = view;
+        const {arch} = genericProps;
+        const archInfo = new ArchParser().parse(arch);
+
+        return {
+            ...genericProps,
+            Model: view.Model,
+            Renderer: view.Renderer,
+            archInfo,
+        };
+    },
+};
+
+registry.category("views").add("CRMLeadMapView", CRMLeadMapView);
