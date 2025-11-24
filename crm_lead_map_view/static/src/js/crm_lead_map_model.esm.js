cb67f7eaf14261fb35dd2db5f9e89cd80acaf1bf
joona-isoaho-futural <joona.isoaho@futural.fi>
2025-11-24 10:18:13 +0200

    Precommit fixes

--- crm_lead_map_view/static/src/js/crm_lead_map_model.esm.js
+++ crm_lead_map_view/static/src/js/crm_lead_map_model.esm.js
@@ -67,7 +67,7 @@ export class CRMLeadMapModel {
         );
 
         const partner_ids = $.map(result.records, function(record) {
-            return record.partner_id
+            return record.partner_id;
         }).filter(function(item) {
             return item;
         });
